67cf43930be8b2967b8a10cd10febd55627578d2
brettspeaks <brett.speaks@dandsltd.com>
2017-06-15 15:09:45 -0500

    adding documentation.

--- node/odbc_inst_macos.sh
+++ node/odbc_inst_macos.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-SRC_ODBC=/usr/local/Cellar/unixodbc/2.3.2_1/include
+# this is a script which manually sets up the paths so that you can install node odbc.
+# unixodbc should should be installed first from homebrew.
+
+SRC_ODBC=/usr/local/Cellar/unixodbc/2.3.4/include
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:$SRC_ODBC
 export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$SRC_ODBC
 npm install odbc
